f072fb57524abd9ae0e69683cfea23d297431fb6
luis cruz <luis_cruz@outlook.com>
2022-09-15 22:48:36 -0600

    Data provider

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Para ejecutarlo debes clonar en proyecto y ejecturar este comando
 ```
 composer install
 ```
-Puedes leer gratis el articulo completo en [Codigo Naranja](https://codigonaranja.com/unit-test-php)
+Puedes leer gratis el articulo completo en [Codigo Naranja](https://codigonaranja.com/unit-test-php-data-provider)
--- tests/ListParserTest.php
+++ tests/ListParserTest.php
@@ -12,31 +12,20 @@ class ListParserTest extends TestCase
         $this->listParser = new ListParser();
     }
 
-
-    public function test_funciona_con_listas_separadas_por_coma(){
-
-        $lista_array = ["vba","c#","php"];
-        $lista_test = $this->listParser->parse("vba,c#,php");         
-
-        $this->assertSame($lista_test,$lista_array);
-         
+    /**
+     * @dataProvider listasDePrueba
+     */
+    public function test_comprobar_listas($lista, $expectativa){         
+        $lista_test = $this->listParser->parse($lista); 
+        $this->assertSame($lista_test, $expectativa);         
     }
 
-    public function test_funciona_con_listas_separadas_por_coma_y_espacio(){
-
-        $lista_array = ["vba","c#","php"];
-        $lista_test = $this->listParser->parse("vba, c#,php");         
-
-        $this->assertSame($lista_test,$lista_array);
-         
-    }
-
-    public function test_funciona_con_listas_separadas_por_coma_y_o_espacio(){
-
-        $lista_array = ["vba","c#","php"];
-        $lista_test = $this->listParser->parse("vba c#,php");         
-
-        $this->assertSame($lista_test,$lista_array);
-         
+    public function listasDePrueba()
+    {
+        return [
+            ["vba,c#,php", ["vba","c#","php"]],
+            ["vba, c#,php",["vba","c#","php"]], 
+            ["vba c#,php",["vba","c#","php"]]   
+        ];
     }
 }
